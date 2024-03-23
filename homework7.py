24676280c7f95352c0bc338a6aa795de458b1c9c
UnrealiSandro <156936975+UnrealiSandro@users.noreply.github.com>
2024-03-23 14:31:42 +0400

    Add files via upload

---
+++ homework7.py
@@ -0,0 +1,27 @@
+a = 22    
+b = 16
+c = "12"
+d = True
+e = 25
+
+
+result = (a > b > (c) == d == e)
+
+print(result)
+
+  
+
+
+
+
+#and operator
+print(True and True)   # True
+print(True and False)  # False
+print(False and True)  # False
+print(False and False) # False
+#or operator
+
+print(True or True)    # True
+print(True or False)   # True
+print(False or True)   # True
+print(False or False)  # False

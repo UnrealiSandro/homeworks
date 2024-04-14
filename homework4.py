0c32406f3c011ee2411c5d44385c45b64645f556
UnrealiSandro <156936975+UnrealiSandro@users.noreply.github.com>
2024-04-14 22:42:08 +0400

    Delete homework4.py

--- homework4.py
+++
@@ -1,7 +0,0 @@
-# Ask the user for input
-first_name = input("Enter your first name: ")
-last_name = input("Enter your last name: ")
-age = input("Enter your age: ")
-
-# Print the information in one sentence
-print(f"gamarjoba, {first_name} {last_name}! shen xar {age} wlis.")

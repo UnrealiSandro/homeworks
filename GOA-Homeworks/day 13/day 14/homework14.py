9211e51fff978a29310b2031171354f9d03c9804
UnrealiSandro <156936975+UnrealiSandro@users.noreply.github.com>
2024-05-11 16:19:29 +0400

    Add files via upload

---
+++ GOA-Homeworks/day 13/day 14/homework14.py
@@ -0,0 +1,8 @@
+# Take user input for a number
+num = int(input("Enter a number: "))
+
+# Check if the number is odd or even
+if num % 2 == 0:
+    print(f"{num} is an even number.")
+else:
+    print(f"{num} is an odd number.")

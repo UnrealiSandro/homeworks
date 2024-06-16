c90bce717025effc68d62f98a73fab3c6850b99d
Sandro <156936975+UnrealiSandro@users.noreply.github.com>
2024-06-16 15:21:56 +0400

    Add files via upload

---
+++ GOA-Homeworks/day 19/homework/homework.py
@@ -0,0 +1,20 @@
+# პროგრამული ენა
+# library
+# heloo world
+# true
+# კომენტარი იყენება hashtag -ით
+# 2.5
+# error
+# 25
+# true
+# break
+# 1
+# true
+# true
+# array
+# მოქმედების ნაბიჯ-ნაბიჯ შესრულება
+# კუდი
+# true
+# false
+# true
+# ფასანური

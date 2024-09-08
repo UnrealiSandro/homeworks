9f01f4a9b2e4f04ebe15cbcebd19ed5ff9358ff3
Sandro <156936975+UnrealiSandro@users.noreply.github.com>
2024-09-08 14:44:38 +0400

    Add files via upload

---
+++ day 53-58 extra homework/day 57/extra57.py
@@ -0,0 +1,140 @@
+# print("Hello, World!")
+
+
+
+# user_input = input("Enter something: ")
+# print(user_input)
+
+
+
+# # This is a single-line comment
+
+# """
+# This is a 
+# multi-line comment
+# """
+
+# print("Comments demonstration")
+
+
+# def check_number(num):
+#     if num > 0:
+#         print("Positive number")
+#     elif num < 0:
+#         print("Negative number")
+#     else:
+#         print("Zero")
+
+# check_number(5)
+
+
+
+# long_string = "This is a long string, " \
+#               "split into multiple lines."
+# print(long_string)
+
+
+
+# # Get input from the user
+# user_name = input("Enter your name: ")
+
+# # Print a greeting message
+# print("Hello, " + user_name)
+
+
+
+
+# # This function greets the user
+# def greet_user(name):
+#     print("Hello, " + name)
+
+# greet_user("Sandro")
+
+
+
+# """
+# This script takes a user's name
+# and greets them with a message.
+# """
+
+# name = input("Enter your name: ")
+# print("Hello, " + name)
+
+
+
+# # This function greets the user
+# def greet_user(name):
+#     print("Hello, " + name)
+
+# greet_user("Sandro")
+
+
+
+# """
+# This script takes a user's name
+# and greets them with a message.
+# """
+
+# name = input("Enter your name: ")
+# print("Hello, " + name)
+
+
+
+
+# # This part of the script is disabled
+# # print("This won't print")
+
+# print("This will print")
+
+
+
+
+# # Error: missing closing parentheses
+# # print("This will cause a syntax error
+
+
+
+
+# name = "sandro"
+# age = 14
+# height = 5.0
+# is_student = True
+
+# print(name, age, height, is_student)
+
+
+
+
+# a = 5
+# b = 10
+# a, b = b, a
+# print(a, b)
+
+
+
+
+
+# name = input("Enter your name: ")
+# age = int(input("Enter your age: "))
+
+# print(name, age)
+
+
+
+
+
+# x = "global"
+
+# def print_variable():
+#     x = "local"
+#     print(x)
+
+# print_variable()
+# print(x)
+
+
+
+
+# user_name = "Sandro"
+# userAge = 14
+# _user = "User"

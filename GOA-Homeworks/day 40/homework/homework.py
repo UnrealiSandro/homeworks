abae00676a939a997492f59d4497cb2bfdd684e4
Sandro <156936975+UnrealiSandro@users.noreply.github.com>
2025-01-28 22:31:45 +0400

    Add files via upload

---
+++ GOA-Homeworks/day 40/homework/homework.py
@@ -0,0 +1,8 @@
+def greed(Sandro):
+    return f"Hello, {Sandro}!"
+
+def sandwich_make(bread, tomato, ham):
+    return f"Tqven Gaaketet Sandwich! {bread}, {tomato}, and {ham}."
+
+print(greed("Sandro"))
+print(sandwich_make("white bread", "tomato", "ham"))

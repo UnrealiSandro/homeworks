2ce9c173ee8bcefa24436d4786de20a36a8cf401
Sandro <156936975+UnrealiSandro@users.noreply.github.com>
2025-02-13 20:34:51 +0400

    Add files via upload

---
+++ GOA-Homeworks/day 44/classwork/classwork.py
@@ -0,0 +1,9 @@
+numbers = [10, 25, 37, 49, 53]
+
+words = ["apple", "banana", "pear", "cat", "dog"]
+
+booleans = [True, False, True, False, True]
+
+decimals = [3.14, 2.71, 1.618, 0.577, 4.669]
+
+mixed = [42, "hello", 3.14, True, [1, 2, 3]]
